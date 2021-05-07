37e23f4b43c45745ac12fe3b9f1d0d3969c2742c
fdis111 <disubif@gmail.com>
2021-05-07 03:21:10 +0200

    feature : add import plugin ui

---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '640fc205-3695-4107-a96a-983a847b70c0'
+};
--- plugins/import-content/admin/src/containers/HomePage/index.js
+++ plugins/import-content/admin/src/containers/HomePage/index.js
@@ -4,15 +4,151 @@
  *
  */
 
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect } from "react";
 // import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+import pluginId from "../../pluginId";
+import UploadFileForm from "../../components/UploadFileForm";
+import {
+  HeaderNav,
+  LoadingIndicator,
+  PluginHeader,
+} from "strapi-helper-plugin";
+import { request } from "strapi-helper-plugin";
+import Row from "../../components/Row";
+import Block from "../../components/Block";
+import { Select, Label } from "@buffetjs/core";
+import { get, has, isEmpty, pickBy, set } from "lodash";
 
 const HomePage = () => {
+  const [analyzing, setAnalyzing] = useState(false);
+  const [analysis, setAnalysis] = useState(null);
+  const [importSource, setimportSource] = useState("upload");
+  const [loading, setLoading] = useState(false);
+  const [modelOptions, setModelOptions] = useState([]);
+  const [models, setmodels] = useState([]);
+  const [selectedContentType, setSelectedContentType] = useState("");
+  const importSources = [
+    { label: "External URL ", value: "url" },
+    { label: "Upload file", value: "upload" },
+    { label: "Raw text", value: "raw" },
+  ];
+  const selectImportDest = (selectedContentType) => {
+    setSelectedContentType(selectedContentType);
+  };
+  const getModels = async () => {
+    setLoading(true);
+    try {
+      const response = await request("/content-type-builder/content-types", {
+        method: "GET",
+      });
+
+      // Remove non-user content types from models
+      const models = get(response, ["data"], []).filter(
+        (obj) => !has(obj, "plugin")
+      );
+      const modelOptions = models.map((model) => {
+        return {
+          label: get(model, ["schema", "name"], ""),
+          value: model.uid,
+        };
+      });
+
+      setLoading(false);
+
+      return { models, modelOptions };
+    } catch (e) {
+      setLoading(false);
+      strapi.notification.error(`${e}`);
+    }
+    return [];
+  };
+  const getUrl = (to) =>
+    to ? `/plugins/${pluginId}/${to}` : `/plugins/${pluginId}`;
+  const analyse = async (analysisConfig) => {
+    try {
+      const response = await request("/import-content/preAnalyzeImportFile", {
+        method: "POST",
+        body: analysisConfig,
+      });
+      setAnalysis(response);
+      setAnalyzing(false);
+      strapi.notification.success(`Analyzed Successfully`);
+    } catch (e) {
+      setAnalyzing(false);
+      strapi.notification.error(`Analyze Failed, try again`);
+      strapi.notification.error(`${e}`);
+    }
+  };
+
+  const onRequestAnalysis = async (analysisConfig) => {
+    // this.analysisConfig = analysisConfig;
+    setAnalyzing(true);
+    analyse(analysisConfig);
+  };
+
+  const selectImportSource = (importSource) => {
+    setimportSource(importSource);
+  };
+
+  useEffect(() => {
+    getModels().then((res) => {
+      const { models, modelOptions } = res;
+      setmodels(models);
+      setModelOptions(modelOptions);
+      setSelectedContentType(modelOptions ? modelOptions[0].value : "");
+    });
+  }, []);
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
+    <div className={"container-fluid"} style={{ padding: "18px 30px" }}>
+      <PluginHeader
+        title={"Import Content"}
+        description={"Import CSV and RSS-Feed into your Content Types"}
+      />
+      <HeaderNav
+        links={[
+          {
+            name: "Import Data",
+            to: getUrl(""),
+          },
+          {
+            name: "Import History",
+            to: getUrl("history"),
+          },
+        ]}
+        style={{ marginTop: "4.4rem" }}
+      />
+      <div className="row">
+        <Block
+          title="General"
+          description="Configure the Import Source & Destination"
+          style={{ marginBottom: 12 }}
+        >
+          <Row className={"row"}>
+            <div className={"col-4"}>
+              <Label htmlFor="importSource">Import Source</Label>
+              <Select
+                name="importSource"
+                options={importSources}
+                value={importSource}
+                onChange={({ target: { value } }) => selectImportSource(value)}
+              />
+            </div>
+            <div className={"col-4"}>
+              <Label htmlFor="importDest">Import Destination</Label>
+              <Select
+                value={selectedContentType}
+                name="importDest"
+                options={modelOptions}
+                onChange={({ target: { value } }) => selectImportDest(value)}
+              />
+            </div>
+          </Row>
+          <UploadFileForm
+            onRequestAnalysis={onRequestAnalysis}
+            loadingAnalysis={analyzing}
+          />
+        </Block>
+      </div>
     </div>
   );
 };
